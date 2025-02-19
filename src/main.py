cca6ba78a190ad6a342515c259ad3c29f441d9d5
Matt Redmond <matt.redmond@katalyst.com.au>
2025-02-20 00:42:18 +1030

    Copy static content to public

--- src/main.py
+++ src/main.py
@@ -1,9 +1,34 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+def clean(target):
+    if os.path.exists(target):
+        shutil.rmtree(target)
+
+def create(target):
+    if not os.path.exists(target):
+        os.mkdir(target)
+
+def copy(source, target):
+    for item in os.listdir(source):
+        if os.path.isfile(os.path.join(source, item)):
+            shutil.copy(os.path.join(source, item), os.path.join(target, item))
+        else:
+            next_source = os.path.join(source, item)
+            next_target = os.path.join(target, item)
+            if not os.path.exists(next_target):
+                os.mkdir(next_target)
+            copy(next_source, next_target)
+
 
 def main():
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    cwd = os.getcwd()
 
-    print(node)
+    source = os.path.join(cwd, "static")
+    target = os.path.join(cwd, "public")
 
+    clean(target)
+    create(target)
+    copy(source, target)
 
 main()
