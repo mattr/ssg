e4afa51d35e17ba900363e706175d72f39371684
Matt Redmond <matt.redmond@katalyst.com.au>
2025-03-03 00:52:07 +1030

    Support underscore for italics

--- src/texttotextnodes.py
+++ src/texttotextnodes.py
@@ -6,9 +6,10 @@ from textnode import TextNode, TextType
 
 def text_to_text_nodes(text):
     nodes = [TextNode(text, TextType.TEXT)]
-    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
-    nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
-    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
     nodes = split_nodes_image(nodes)
     nodes = split_nodes_link(nodes)
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
     return nodes
